9bd39a2e03a5ab9383c02f9e2c5761f48f52d7c9
israelGitV1 <kasper.war@gmail.com>
2024-10-03 19:51:41 -0300

    :sparkles: feat(desafio): completando o desafio DIO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
-﻿using DesafioPOO.Models;
+﻿using System.Reflection.Metadata;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone i = new Iphone(numero:"998583540",modelo:"S13",imei:"User@gmail.com",memoria:64);
+Nokia n = new Nokia(numero:"988405091",modelo:"N34",imei:"User2@gamil.com",memoria:8);
+
+System.Console.WriteLine("\n----------------------Iphone---------------------------");
+i.Ligar();
+i.ReceberLigacao();
+i.InstalarAplicativo("Youtube");
+System.Console.WriteLine("\n-----------------------Nokia--------------------------");
+n.Ligar();
+n.ReceberLigacao();
+n.InstalarAplicativo("Youtube");
